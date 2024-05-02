9fcd185fe180e3d565044a631eeabf05191eb30d
mikesplore <mikepremium8@gmail.com>
2024-05-02 21:59:50 +0300

    start

---
+++ app/src/main/java/com/example/dylan/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.dylan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.dylan.ui.theme.DylanTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+                }
+            }
+        }
+
+@Composable
+fun Main(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "welcome") {
+
+    }
+}
