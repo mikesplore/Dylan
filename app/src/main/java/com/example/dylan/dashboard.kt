8676030a5285802bf323e2a892c7740fdf5cedcf
mikesplore <mikepremium8@gmail.com>
2024-05-03 12:55:13 +0300

    p2

--- app/src/main/java/com/example/dylan/dashboard.kt
+++ app/src/main/java/com/example/dylan/dashboard.kt
@@ -1,11 +1,57 @@
 package com.example.dylan
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
+
 
 @Composable
 fun Dashboard(){
-    Column {
-
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet { /* Drawer content */ }
+        },
+    ) {
+        Scaffold(
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    text = { Text("Show drawer") },
+                    icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+                    onClick = {
+                        scope.launch {
+                            drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    }
+                )
+            }
+        ) { contentPadding ->
+            // Screen content
+            Text(text = "Hello there")
+        }
     }
+
+}
+
+@Preview
+@Composable
+fun dashboardpreview(){
+    Dashboard()
 }
